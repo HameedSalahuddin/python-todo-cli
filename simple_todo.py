71ed7ff5f57456c1d53641d47515eef2c8888a58
hameed <hameed.work08@gmail.com>
2026-05-25 19:42:46 +0300

    added savefile and readfile functions

--- simple_todo.py
+++ simple_todo.py
@@ -6,12 +6,25 @@ print('3.remove tasks')
 print('4.exit')
 def save_file():
     file = open('task.txt','w')
-    file.write(str(tasks))
+    for i in tasks:
+        file.write(i)
+        file.write('\n')
+
     file.close()
+def read_file():
+    file = open('task.txt','r')
+    lst = file.readlines()
+    for i in lst:
+        tasks.append(i.strip())
+
+'''when the code stops and is run again the task list will be empty 
+   so we use the read_file() funtion to read the tasks uploaded into the 
+   textfile and add it back into the tasks[] list so the tasks added before
+    will be present in it'''
+read_file()
 while True:
     opt = input('enter operation :')
     if opt == '1':
-        
         for num , item in enumerate(tasks,start=1):
 
             print(num,'.',item)
@@ -19,6 +32,7 @@ while True:
         if done == 'y':
             n = int(input('enter the task which is done : '))
             tasks[n-1]= tasks[n-1]+''+'[DONE]'
+            save_file()
             print('GOOD JOB!!!!')
 
 
@@ -33,11 +47,12 @@ while True:
         save_file()
 
     elif opt=='3':
-        for i in tasks:
-            print(i)
-        item = input('enter the task that you want to delete :')
-        x = tasks.index(item)
-        tasks.pop(x)
+        for i,item in enumerate(tasks,start=1):
+
+            print(i,'.',item)
+        delete = int(input('enter the task that you want to delete :'))
+        tasks.pop(delete-1)
+        save_file()
         print('successfully deleted')
     else:
-        break
+        break 
